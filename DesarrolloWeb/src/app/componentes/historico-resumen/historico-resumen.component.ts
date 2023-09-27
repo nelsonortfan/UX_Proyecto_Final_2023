c71915537d5deebab8c7b09937392d0db77899b8
Nelson <n.ortizf@uniandes.edu.co>
2023-09-27 12:13:35 -0500

    pantalla reportes historicos ok

--- DesarrolloWeb/src/app/componentes/historico-resumen/historico-resumen.component.ts
+++ DesarrolloWeb/src/app/componentes/historico-resumen/historico-resumen.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
+
+
+
+const THUMBUP_ICON =
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512">
+  <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License
+  - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+  <path d="M488 261.8C488 403.3 391.1 504 248
+  504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0
+  123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6
+  94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4
+  140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>
+`;
 
 @Component({
   selector: 'app-historico-resumen',
@@ -7,9 +23,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoricoResumenComponent implements OnInit {
 
-  constructor() { }
+  seasons: String[] = ['Última Semana', 'Últimos 15 Días', 'Último Mes', 'Último Semestre'];
+  checked = false;
+  indeterminate = false;
+
+
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('thumbs-up', sanitizer.bypassSecurityTrustHtml(THUMBUP_ICON));
+  }
 
   ngOnInit(): void {
+
   }
 
 }
